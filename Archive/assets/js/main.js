70ea0a8b5657ac2f2004f014037b55e0c194c96e
isabellageorgakis <69996700+isabellageorgakis@users.noreply.github.com>
2020-10-14 16:56:09 +1100

    Upload Archive

---
+++ Archive/assets/js/main.js
@@ -0,0 +1,52 @@
+$ (document).ready(function(){
+	console.log('hello world');
+
+	$('.btn-keyhole').click(function(){
+		$('.keyhole').addClass('active');
+		$('.btn-keyhole').addClass('remove');
+	});
+
+	$('.btn-nav').click(function(){
+		$('.nav').toggleClass('active');
+	});
+
+	$('.btn-ticket').click(function(){
+		$('.item').addClass('hide');
+		$('.item.ticket').removeClass('hide');
+	});
+
+	$('.btn-key').click(function(){
+		$('.item').addClass('hide');
+		$('.item.key').removeClass('hide');
+	});
+
+	$('.btn-coin').click(function(){
+		$('.item').addClass('hide');
+		$('.item.coin').removeClass('hide');
+	});
+
+	$('.btn-arts').click(function(){
+		$('.item').addClass('hide');
+		$('.item.arts').removeClass('hide');
+	});
+
+	$('.btn-gardens').click(function(){
+		$('.item').addClass('hide');
+		$('.item.gardens').removeClass('hide');
+	});
+
+	$('.btn-sport').click(function(){
+		$('.item').addClass('hide');
+		$('.item.sport').removeClass('hide');
+	});
+
+	$('.btn-transport').click(function(){
+		$('.item').addClass('hide');
+		$('.item.transport').removeClass('hide');
+	});
+
+	$('.btn-all').click(function(){
+		$('.item').removeClass('hide');
+	});
+
+});
