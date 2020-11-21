34388a2cd37c2f3513bef23e9da954058f147609
isabellageorgakis <isabellageorgakis@hotmail.com>
2020-11-22 10:43:15 +1100

    js update

--- notabook/assets/js/main.js
+++ notabook/assets/js/main.js
@@ -1,37 +1,39 @@
 	$ (document).ready(function(){
-		console.log('ready')
+		console.log('ready');
 
-		$('.btn-resent').click(function(){
+		$('.btn-reset').click(function(){
 			$('.article').removeClass('hide');
-		})
+		});
 
 		$('.btn-politics').click(function(){
 			$('.article').addClass('hide');
-			$('.article.politics').removeClass('hide')
-		})
+			$('.article.politics').removeClass('hide');
+		});
 
 		$('.btn-business').click(function(){
 			$('.article').addClass('hide');
-			$('.article.business').removeClass('hide')
-		})
+			$('.article.business').removeClass('hide');
+		});
 
 		$('.btn-tech').click(function(){
 			$('.article').addClass('hide');
-			$('.article.tech').removeClass('hide')
-		})
+			$('.article.tech').removeClass('hide');
+		});
 
 		$('.btn-science').click(function(){
 			$('.article').addClass('hide');
-			$('.article.science').removeClass('hide')
-		})
+			$('.article.science').removeClass('hide');
+		});
 
 		$('.btn-healthbeauty').click(function(){
 			$('.article').addClass('hide');
-			$('.article.healthbeauty').removeClass('hide')
-		})
+			$('.article.healthbeauty').removeClass('hide');
+		});
 
 		$('.btn-celebrity').click(function(){
 			$('.article').addClass('hide');
-			$('.article.celebrity').removeClass('hide')
-		})
+			$('.article.celebrity').removeClass('hide');
+		});
+
+		});
 
