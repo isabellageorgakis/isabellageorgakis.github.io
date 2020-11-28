cef1d623d338ff519b9324464e34132aa485bc60
isabellageorgakis <isabellageorgakis@hotmail.com>
2020-11-28 16:54:51 +1100

    Design Updates

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -9,5 +9,38 @@ $ (document).ready(function(){
 		$('.contact').removeClass('active');
 	})
 
+	$('.btn-works').click(function(){
+		$('.works').addClass('active');
+	})
+
+	$('.x').click(function(){
+		$('.works').removeClass('active');
+	})
+
+	$('.btn-NFN').click(function(){
+		$('.image').addClass('hide');
+		$('.image.NFN').removeClass('hide');
+	});
+
+	$('.btn-ES').click(function(){
+		$('.image').addClass('hide');
+		$('.image.ES').removeClass('hide');
+	});
+
+	$('.btn-OBS').click(function(){
+		$('.image').addClass('hide');
+		$('.image.OBS').removeClass('hide');
+	});
+
+	$('.btn-SF').click(function(){
+		$('.image').addClass('hide');
+		$('.image.SF').removeClass('hide');
+	});
+
+	$('.btn-EA').click(function(){
+		$('.image').addClass('hide');
+		$('.image.EA').removeClass('hide');
+	});
+
 })
 
