77f96ffca04baba15e89c9ee2da5028820dd472e
isabellageorgakis <isabellageorgakis@hotmail.com>
2021-02-17 16:32:02 +1100

    Files Moved

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+$ (document).ready(function(){
+	console.log('ready');
+
+	$('.btn-contact').click(function(){
+		$('.contact').addClass('active');
+	})
+
+	$('.close').click(function(){
+		$('.contact').removeClass('active');
+	})
+
+	$('.btn-works').click(function(){
+		$('.works').addClass('active');
+	})
+
+	$('.x').click(function(){
+		$('.works').removeClass('active');
+	})
+
+	$('.btn-NFN').click(function(){
+		$('.image').addClass('hide');
+		$('.image.NFN').removeClass('hide');
+	});
+
+	$('.btn-ES').click(function(){
+		$('.image').addClass('hide');
+		$('.image.ES').removeClass('hide');
+	});
+
+	$('.btn-OBS').click(function(){
+		$('.image').addClass('hide');
+		$('.image.OBS').removeClass('hide');
+	});
+
+	$('.btn-SF').click(function(){
+		$('.image').addClass('hide');
+		$('.image.SF').removeClass('hide');
+	});
+
+	$('.btn-EA').click(function(){
+		$('.image').addClass('hide');
+		$('.image.EA').removeClass('hide');
+	});
+
+	$('.btn-AA').click(function(){
+		$('.image').addClass('hide');
+		$('.image.AA').removeClass('hide');
+	});
+
+	$('.btn-WW').click(function(){
+		$('.image').addClass('hide');
+		$('.image.WW').removeClass('hide');
+	});
+
+	$('.main').click(function(){
+		$('.image').removeClass('hide');
+	});
+
+})
+
